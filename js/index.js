e4ed9e803a7e9c45f8fce7ceaee0f1560c110bd3
clay <1150689758@qq.com>
2019-01-02 20:19:22 +0800

    bugfix

--- js/index.js
+++ js/index.js
@@ -1,108 +1,22 @@
-// 发送GET请求
-function $get(url,callback) {
+// 请求歌单
+function getMusicList(callback) {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET',url,true);
+  xhr.open('GET','//easy-mock.com/mock/5ad86860c1196e47fdb233cc/mock/musicPlayer',true);
   xhr.send();
   xhr.onload = function() {
     if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
-      // console.log(JSON.parse(xhr.responseText));
       callback(xhr.responseText);
     } else {
       console.log('server error')
     }
   }
 }
-// 请求歌单
-function getMusicList(id) {
-  $get('//api.imjad.cn/cloudmusic/?type=playlist&id='+id,formatMusicList)
-}
-// 处理歌曲详细信息
-function songDetailStorage(res) {
-  song = JSON.parse(res).songs[0];
-  var storage = window.localStorage;
-  var obj = {};
-  var temp = localStorage.getItem(song.id);
-  temp == undefined ? 
-  obj = Object.assign({},{
-    name: song.name,
-    pic: song.al.picUrl,
-    author: song.ar[0].name
-  }) 
-  : 
-  obj = Object.assign(JSON.parse(temp),{
-    name: song.name,
-    pic: song.al.picUrl,
-    author: song.ar[0].name
-  });
-  storage.setItem(song.id,JSON.stringify(obj));
-  // if(isComplete(song.id)) {
-  //   initAudio(song.id);
-  // }
-}
-// 获取音频文件地址
-function songUrlStorage(res) {
-  var song = JSON.parse(res).data[0];
-  var obj = {};
-  var storage = window.localStorage;
-  var temp = localStorage.getItem(song.id);
-  temp == undefined ? 
-  obj = Object.assign({},{url: song.url}) 
-  : 
-  obj = Object.assign(JSON.parse(temp),{url: song.url});
-  storage.setItem(song.id,JSON.stringify(obj));
-  // if(isComplete(song.id)) {
-  //   initAudio(song.id);
-  // }
-}
-// 处理歌单信息
-function formatMusicList (res) {
-  console.log(res)
-  var storage = window.localStorage;
-  window.songIds = [];
-  JSON.parse(res).playlist.trackIds.forEach(function(item){
-    if(!storage.getItem(item.id)) {
-      window.songIds.push(item.id);
-      storage.setItem('songIds',JSON.stringify(songIds))
-      $get('//api.imjad.cn/cloudmusic/?type=detail&id='+item.id,songDetailStorage);
-      $get('//api.imjad.cn/cloudmusic/?type=song&id='+item.id,songUrlStorage);
-    }
-  })
- }
- // 判断歌曲信息是否完整
- function isComplete(id) {
-  var temp = JSON.parse(window.localStorage.getItem(id));
-  if(!!temp &&!!temp.url && !!temp.name) {
-    return true
-  } else if(!!temp && !temp.url){
-    $get('//api.imjad.cn/cloudmusic/?type=song&id='+id,songUrlStorage);
-    return false;
-  } else if (!!temp && !temp.name) {
-    $get('//api.imjad.cn/cloudmusic/?type=detail&id='+id,songDetailStorage);
-    return false;
-  } else {
-    $get('//api.imjad.cn/cloudmusic/?type=song&id='+id,songUrlStorage);
-    $get('//api.imjad.cn/cloudmusic/?type=detail&id='+id,songDetailStorage);
-    return false;
-  }
- }
+
  // init audio 
  function initAudio() {
-  var storage = window.localStorage;
-  var songIds = JSON.parse(storage.getItem('songIds')).filter(function(id){
-    return isComplete(id)
-  });
-  console.log(songIds)
-  var songArray = [];
-  songIds.forEach(function(item){
-    var song = JSON.parse(storage.getItem(item));
-    songArray.push(song);
-  })
-  console.log("songArray:\n",songArray)
-  // storage.setItem('songIds',JSON.stringify(songIds));
-  // var song = JSON.parse(storage.getItem(id ? id : songIds[0]));
-  // window.audioObject = new Audio(song.url);
-  // window.currentSong = index;
-  // audioObject.autoPlay = true; 
+  window.audioObject = new Audio('https://m8.music.126.net/20190101215025/2531b7f86cd3f49d04a325d8511273cc/ymusic/2987/5611/69c5/16ef39280e7cf1ecd8a02250ebc91d07.mp3');
+  window.currentSong = index;
+  audioObject.autoPlay = true; 
 }
 // song detail info
 function changeInfo(song) {
